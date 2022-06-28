481e8f190b1cd0999c21f79757b0891be747e065
Adi-Marian Mutu <39502043+AdiMarianMutu@users.noreply.github.com>
2022-06-29 02:21:42 +0300

    frist commit

---
+++ src/telegram-theme-animation.js
@@ -0,0 +1,40 @@
+window.currentTheme = 'light';
+
+(() => {
+  document.getElementById('theme-animation').addEventListener('animationend', function () {
+    this.children[0].remove();
+    this.classList.remove('theme-animation_do');
+  });
+})();
+
+function toggleTheme() {
+  const ROOT = document.getElementsByTagName('html')[0];
+  const BODY = document.body;
+  const VIEW = document.getElementById('theme-animation');
+  const btnThemeToggler = document.getElementById('theme-toggler');
+
+  if (!document.getElementById('theme-animation')?.classList.contains('theme-animation_do')) {
+    html2canvas(ROOT, {
+      allowTaint: true,
+      removeContainer: true,
+      logging: false,
+    }).then((canvas) => {
+      VIEW.appendChild(canvas);
+
+      if (window.currentTheme === 'light') {
+        window.currentTheme = 'dark';
+        btnThemeToggler.value = 'Light Mode';
+        BODY.style.backgroundColor = 'black';
+        BODY.style.color = 'white';
+      } else {
+        window.currentTheme = 'light';
+        btnThemeToggler.value = 'Dark Mode';
+        // btnThemeToggler?.style.width = '100px';
+        BODY.style.backgroundColor = 'white';
+        BODY.style.color = 'black';
+      }
+
+      VIEW.classList.add('theme-animation_do');
+    });
+  }
+}
